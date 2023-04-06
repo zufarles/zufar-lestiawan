03ba2d246f83494ee3692ac685bc4636691d7686
zufarles <128969744+zufarles@users.noreply.github.com>
2023-04-06 18:07:37 +0700

    Add files via upload

---
+++ Latihan/CobaMhs.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Latihan;
+
+/**
+ *
+ * @author Administrator
+ */
+public class CobaMhs {
+ 
+     public static void main(String[] args) {
+        Mhs mhszufar = new Mhs();
+        mhszufar.setNama("Zufar Lestiawan");
+        System.out.print("Nama Mhs : " + mhszufar.getNama());
+    }
+} 
+
+
---
+++ Latihan/Mhs.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Latihan;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Mhs {
+    private String nama;
+    private float ipk;
+
+    // behavior
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setIpk(float ipk) {
+        this.ipk = ipk;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+}   
+
+ 
+
